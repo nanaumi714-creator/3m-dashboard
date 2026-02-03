60ed7de9523a9fcf3172b2c57cfe6e0d10167edc
nanaumi714-creator <nanaumi.714@gmail.com>
2026-02-03 13:11:36 +0900

    Fix mobile transactions swipeable import

--- web/app/mobile/transactions/page.tsx
+++ web/app/mobile/transactions/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabase";
-import SwipeableItem from "../components/SwipeableItem";
+import SwipeableItem from "../../(dashboard)/components/SwipeableItem";
 
 type Transaction = {
     id: string;
